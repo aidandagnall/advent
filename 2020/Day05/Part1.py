3cbb82131eac068af58077d3b71e3eb50aad8e5b
Aidan Dagnall <psyad12@nottingham.ac.uk>
2020-12-05 06:30:17 +0000

    🎨 Niceify Day 5

--- 2020/Day05/Part1.py
+++ 2020/Day05/Part1.py
@@ -1,9 +1,7 @@
 def getCoordinates(str):
-    row, col = str[:7], str[7:]
-    row = row.replace('F', '0').replace("B", "1")
-    col = col.replace("L", "0").replace("R", "1")
-    return (int(row, 2), int(col, 2))
+    str = ''.join('0' if i in 'FL' else '1' for i in str)
+    return (int(str, 2))
     
 passes = [i.strip() for i in open('input.txt', 'r')]
-seatnumbers = [(getCoordinates(i)[0] * 8 + getCoordinates(i)[1]) for i in passes]
+seatnumbers = [getCoordinates(i) for i in passes]
 print(max(seatnumbers))
