480a1ab1d5ce4e1c0f3e20256acb10c1b6fd5eab
Aidan Dagnall <aidandagnall@gmail.com>
2020-12-11 18:29:09 +0000

    ⚡️ Niceify and improve efficiency for Day 11

--- 2020/Day11/Part1.py
+++ 2020/Day11/Part1.py
@@ -1,35 +1,30 @@
-import copy
-def getadjacent(arr, x, y):
+def getadjacentcoords(arr, x ,y):
     adjacent = []
     for i in range(y - 1, y + 2):
-        if not 0 <= i < len(arr): continue
+        if not i in range(len(arr)): continue
         for j in range(x - 1, x + 2):
-            if not 0 <= j < len(arr[0]): continue
+            if not j in range(len(arr[0])): continue
             if i == y and j == x: continue
-            adjacent.append(arr[i][j])
+            adjacent.append((i,j))
     return adjacent
 
+def runsimulation(arr):
+    next = []
+    for i, row in enumerate(arr):
+        next.append([])
+        for j, col in enumerate(row):
+            adjacent = [arr[x][y] for x, y in neighbours[i][j]]
+            if col == 'L' and adjacent.count('#') == 0: next[i].append('#')
+            elif col == '#' and adjacent.count('#') >= 4: next[i].append('L')
+            else: next[i].append(col)
+    return next
+
 input = [[j for j in i.strip()] for i in open('input.txt')]
-stable = False
-current = input
-next = copy.deepcopy(current)
-while not stable:
-    stable = True
-    for i in current:
-        print(''.join(i).strip())
-    print()
+neighbours = [[getadjacentcoords(input, j, i) for j in range(len(input[i]))] for i in range(len(input))]
+current = []
+next = input
 
-    for i in range(0, len(input)):
-        for j in range(0, len(input[i])):
-            if current[i][j] == '.': continue
-            adjacent = getadjacent(current, j,i)
-            if current[i][j] == 'L' and adjacent.count('#') == 0:
-                stable = False
-                next[i][j] = '#'
-            if current[i][j] == '#' and adjacent.count('#') >= 4:
-                stable = False
-                next[i][j] = 'L'
-    current = copy.deepcopy(next)
-
-print(sum(i.count('#') for i in current))
-        
+while current != next:
+    current = next
+    next = runsimulation(current)
+print(sum(i.count('#') for i in next))
--- 2020/Day11/Part2.py
+++ 2020/Day11/Part2.py
@@ -1,37 +1,35 @@
-import copy
-def getvisible(arr, x, y):
-    visible = []
+input = [[j for j in i.strip()] for i in open('input.txt')]
+height, width = range(len(input)), range(len(input[0]))
+def getadjacentcoords(arr, x ,y):
+    adjacent = []
     for i in range(y - 1, y + 2):
-        if not 0 <= i < len(arr): continue
+        if not i in range(len(arr)): continue
         for j in range(x - 1, x + 2):
-            if not 0 <= j < len(arr[0]): continue
+            if not j in range(len(arr[0])): continue
             if i == y and j == x: continue
-            a, b = i, j
-            while arr[a][b] == '.':
-                a += i - y
-                b += j - x
-                if not a in range(len(arr)) or not b in range(len(arr[0])): break
-            if not a in range(len(arr)) or not b in range(len(arr[0])): continue
-            visible.append(arr[a][b])
-    return visible
+            a,b = i,j
+            while a+(i-y) in height and b+(j-x) in width and arr[a][b] == '.':
+                a, b = a + (i-y), b + (j-x)
+            adjacent.append((a,b))
+    return adjacent
+
+def runsimulation(arr):
+    next = []
+    for i, row in enumerate(arr):
+        next.append([])
+        for j, col in enumerate(row):
+            adjacent = [arr[x][y] for x, y in neighbours[i][j]]
+            if col == 'L' and adjacent.count('#') == 0: next[i].append('#')
+            elif col == '#' and adjacent.count('#') >= 5: next[i].append('L')
+            else: next[i].append(col)
+    return next
 
 input = [[j for j in i.strip()] for i in open('input.txt')]
-stable = 0
-current = input
-next = copy.deepcopy(current)
-while (stable == 0):
-    stable = 1
-    for i in range(0, len(input)):
-        for j in range(0, len(input[i])):
-            if current[i][j] == '.': continue
-            visible = getvisible(current, j,i)
-            if current[i][j] == 'L' and visible.count('#') == 0:
-                stable = 0
-                next[i][j] = '#'
-            if current[i][j] == '#' and visible.count('#') >= 5:
-                stable = 0
-                next[i][j] = 'L'
-    current = copy.deepcopy(next)
+neighbours = [[getadjacentcoords(input, j, i) for j in width] for i in height]
+current = []
+next = input
 
-print(sum(i.count('#') for i in current))
-        
+while current != next:
+    current = next
+    next = runsimulation(current)
+print(sum(i.count('#') for i in next))
