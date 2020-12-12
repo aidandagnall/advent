5e7e9810b010e5a10d06ab64eb888e8fb0fa7d75
Aidan Dagnall <psyad12@nottingham.ac.uk>
2020-12-12 18:08:39 +0000

    ✨ Add Day 12

---
+++ 2020/Day12/Part1.py
@@ -0,0 +1,23 @@
+input = [(i[0], int(i[1:])) for i in open('input.txt')]
+pos = [0,0]
+dir = 1
+dirs = [[0,1], [1, 0], [0, -1], [-1, 0]]
+
+for instruction, value in input:
+    if instruction == 'N':
+        pos[1] += value
+    elif instruction == 'S':
+        pos[1] -= value
+    elif instruction == 'E':
+        pos[0] += value
+    elif instruction == 'W':
+        pos[0] -= value
+    elif instruction == 'L':
+        dir = (dir - int(value / 90)) % 4
+    elif instruction == 'R':
+        dir = (dir + int(value / 90)) % 4
+    elif instruction == 'F':
+        pos[0] += dirs[dir][0] * value
+        pos[1] += dirs[dir][1] * value
+print(f'x={pos[0]} y={pos[1]} dist={abs(pos[0]) + abs(pos[1])} ')
+
---
+++ 2020/Day12/Part2.py
@@ -0,0 +1,27 @@
+input = [(i[0], int(i[1:])) for i in open('input.txt')]
+pos = [0,0]
+waypoint = [10, 1]
+
+def rotate(arr, n):
+    if n == 0: return arr
+    a, b = -arr[0], arr[1]
+    return rotate([b, a], n - 1)
+
+for instruction, value in input:
+    if instruction == 'N':
+        waypoint[1] += value
+    elif instruction == 'S':
+        waypoint[1] -= value
+    elif instruction == 'E':
+        waypoint[0] += value
+    elif instruction == 'W':
+        waypoint[0] -= value
+    elif instruction == 'L':
+        waypoint = rotate(waypoint, 4 - int(value / 90))
+    elif instruction == 'R':
+        waypoint = rotate(waypoint, int(value / 90))
+    elif instruction == 'F':
+        pos[0] += waypoint[0] * value
+        pos[1] += waypoint[1] * value
+print(f'x={pos[0]} y={pos[1]} dist={abs(pos[0]) + abs(pos[1])} ')
+
