3640150d79728c61035e78392bb75a4d4fe1e5cb
Aidan Dagnall <aidandagnall@gmail.com>
2020-12-14 18:00:42 +0000

    🎨 Niceify Day 14

--- 2020/Day14/Part1.py
+++ 2020/Day14/Part1.py
@@ -1,28 +1,16 @@
 def edit_memory(mask, address, value):
     bin_val = '{0:036b}'.format(value)
-    output = ''
-    for i, c in enumerate(mask):
-        if c == 'X':    output += bin_val[i]
-        else:           output += c
+    output = "".join([c if c !='X' else bin_val[i] for i,c in enumerate(mask)])
     mem[address] = int(output,2)
 
-masks = []
 input = [i.strip() for i in open('input.txt', 'r')]
-mask =  { 'mask': "", 'mem': [] }
+mem = {}
+mask = ''
 for line in input:
     if 'mask' in line:
-        if mask != { 'mask': "", 'mem': [] }: 
-            masks.append(mask)
-            mask =  { 'mask': "", 'mem': [] }
-        mask['mask'] = line.split(' = ')[1]
+        mask = line.split(' = ')[1]
     else:
         arr = line.split(' = ')
-        mask['mem'].append((int(arr[0][4:-1]), int(arr[1])))
-masks.append(mask)
-        
-mem = {}
-for m in masks:
-    for index, value in m['mem']:
-        edit_memory(m['mask'], index, value)
+        edit_memory(mask, int(arr[0][4:-1]), int(arr[1]))
 
 print(sum(mem.values()))
