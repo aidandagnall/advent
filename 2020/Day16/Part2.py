63a5e0e9a1fae2a380a4b5171eadd5b966894643
Aidan Dagnall <aidandagnall@gmail.com>
2020-12-16 13:07:42 +0000

    🎨 Remove unnecessary casts

--- 2020/Day16/Part2.py
+++ 2020/Day16/Part2.py
@@ -13,9 +13,8 @@ with open('input.txt', 'r') as f:
     rules = []
     for line in f:
         if line.strip() == '': break
-        matches = [int(i) for i in re.findall('[0-9]+', line.strip())]
-        rulerange = list(range(matches[0], matches[1] + 1)) + list(range(matches[2], matches[3] + 1))
-        rules.append(rulerange)
+        m = [int(i) for i in re.findall('[0-9]+', line.strip())]
+        rules.append(list(range(m[0], m[1] + 1)) + list(range(m[2], m[3] + 1)))
     f.readline()
     myticket = [int(x) for x in re.findall('[0-9]+', f.readline().strip())]
     f.readline()
@@ -27,7 +26,7 @@ valid_cols = []
 for j, r in enumerate(rules):
     valid_cols.append([])
     for i in range(0, len(myticket)):
-        if all(int(t[i]) in r for t in tickets):
+        if all(t[i] in r for t in tickets):
             valid_cols[j].append(i)
 
 # Working from the rule with the fewest (1) valid columns, work backwards to
