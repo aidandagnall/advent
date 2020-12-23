b9ffcfb86a1a2b38581ff167a2a1386f6a011198
Aidan Dagnall <psyad12@nottingham.ac.uk>
2020-12-23 02:47:17 +0000

    🎨 Rewrite Day 19 Part 1 to use regex

--- 2020/Day19/Part1.py
+++ 2020/Day19/Part1.py
@@ -1,27 +1,10 @@
 import re
-import itertools
 
 def get_rule(a):
-    if rules[a] in ['a', 'b']:
-        return rules[a]
-    L = []
     r = rules[a]
-    if '|' in r:
-        l = get_rule(r[0])
-        for i in r[1 : r.index('|')]:
-            l = list(itertools.product(l, get_rule(i)))
-        m = get_rule(r[r.index('|') + 1])
-        for i in r[r.index('|') + 2 : ]:
-            m = list(itertools.product(m, get_rule(i)))
-        for i in l + m:
-            L.append(''.join(i))
-        return L
-    l = get_rule(r[0])
-    for i in r[1:]:
-        l = list(itertools.product(l, get_rule(i)))
-    for i in l:
-        L.append(''.join(i))
-    return L
+    if r in ['a', 'b']:
+        return r
+    return '(' + ''.join('|' if c == '|' else get_rule(c) for c in r) + ')'
 
 rules = {}
 messages = []
@@ -35,5 +18,5 @@ with open('input.txt') as f:
         if "b" in line[1]: rules[int(line[0])] = "b"
     messages = [i.strip() for i in f]
 
-rule = get_rule(0)
-print(len([i for i in messages if i in rule]))
+rule = get_rule(0) + '\Z'
+print(len([i for i in messages if re.match(rule, i)]))
