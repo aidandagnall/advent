0d14f1b048d7d58e4c49f68984174254bcadb5d0
Aidan Dagnall <aidandagnall@gmail.com>
2021-11-30 19:39:39 +0000

    :white_check_mark: Add test template

---
+++ 2021/src/test/kotlin/days/Day0Test.kt
@@ -0,0 +1,19 @@
+package days
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Day0Test {
+    private val day = Day0()
+
+    @Test
+    fun testPartOne() {
+        assertEquals(day.part1(), 0)
+    }
+
+
+    @Test
+    fun testPartTwo() {
+        assertEquals(day.part2(), 0)
+    }
+}
