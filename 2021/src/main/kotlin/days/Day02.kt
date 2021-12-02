cabe0c5e46c85863d3750b3bc971eb931d81eef5
Aidan Dagnall <aidandagnall@gmail.com>
2021-12-02 06:15:26 +0000

    :art: Add lambda deconstructors to Day 2

--- 2021/src/main/kotlin/days/Day02.kt
+++ 2021/src/main/kotlin/days/Day02.kt
@@ -4,11 +4,11 @@ class Day02 : Day(2) {
     override fun part1() : Any {
         var horiz = 0
         var depth = 0
-        inputSplitList.forEach {
-            when (it[0]) {
-                "forward" -> horiz += it[1].toInt()
-                "down"    -> depth += it[1].toInt()
-                "up"      -> depth -= it[1].toInt()
+        inputSplitList.forEach { (word, value) ->
+            when (word) {
+                "forward" -> horiz += value.toInt()
+                "down"    -> depth += value.toInt()
+                "up"      -> depth -= value.toInt()
             }
         }
         return horiz * depth
@@ -18,14 +18,14 @@ class Day02 : Day(2) {
         var horiz = 0
         var depth = 0
         var aim = 0
-        inputSplitList.forEach {
-            when(it[0]) {
+        inputSplitList.forEach { (word, value) ->
+            when(word) {
                 "forward" -> {
-                    horiz += it[1].toInt()
-                    depth += aim * it[1].toInt()
+                    horiz += value.toInt()
+                    depth += aim * value.toInt()
                 }
-                "down" -> aim += it[1].toInt()
-                "up"   -> aim -= it[1].toInt()
+                "down" -> aim += value.toInt()
+                "up"   -> aim -= value.toInt()
             }
         }
         return horiz * depth
