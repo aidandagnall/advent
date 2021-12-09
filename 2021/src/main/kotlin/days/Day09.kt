d014c872445524befb55fa7ddfe423ac9de035c3
Aidan Dagnall <aidandagnall@gmail.com>
2021-12-09 14:42:28 +0000

    :sparkles: Add day 9 part 2. Read the problem next time.

--- 2021/src/main/kotlin/days/Day09.kt
+++ 2021/src/main/kotlin/days/Day09.kt
@@ -1,27 +1,49 @@
 package days
 
 class Day09 : Day(9) {
-    override fun part1() : Any {
-        val input = inputList.map { it.chunked(1).map { i -> i.toInt() } }
 
+    private val input = inputList.map { it.chunked(1).map { i -> i.toInt() } }
+
+    override fun part1() : Any {
         return input.mapIndexed { i, p ->
                 p.mapIndexed { j, q ->
-                    if (input.adjacent(i, j).all { q < it }) q + 1 else 0
+                    if (input.adjacent(i, j).all { q < it.second }) q + 1 else 0
                 }
             }.flatten()
             .sum()
     }
 
     override fun part2() : Any {
-        return 0
+        val lows = mutableSetOf<Pair<Int, Int>>()
+        input.forEachIndexed { i, p ->
+            p.forEachIndexed { j, q ->
+                if (input.adjacent(i, j).all { q < it.second }) lows.add(i to j)
+            }
+        }
+        return lows.map { flood(setOf(it)).size }
+            .sorted()
+            .takeLast(3)
+            .fold(1) { acc, next -> acc * next }
     }
 
-    private fun <E> List<List<E>>.adjacent(x : Int, y : Int) : List<E> {
-        val adjacent = mutableListOf<E>()
-        if (x - 1 in indices) adjacent.add(this[x - 1][y])
-        if (x + 1 in indices) adjacent.add(this[x + 1][y])
-        if (y - 1 in this[0].indices) adjacent.add(this[x][y - 1])
-        if (y + 1 in this[0].indices) adjacent.add(this[x][y + 1])
+    private fun flood( currentBasin : Set<Pair<Int, Int>> ) : Set<Pair<Int, Int>> {
+        val newBasin = currentBasin.toMutableSet()
+        currentBasin.forEach { (x, y) ->
+            input.adjacent(x, y).forEach { (position , value) ->
+                if (value < 9) newBasin.add(position)
+            }
+        }
+
+        if (currentBasin.size == newBasin.size) return newBasin
+        return flood(newBasin)
+    }
+
+    private fun <E> List<List<E>>.adjacent(x : Int, y : Int) : List<Pair<Pair<Int, Int>, E>> {
+        val adjacent = mutableListOf<Pair<Pair<Int, Int>, E>>()
+        if (x - 1 in indices) adjacent.add((x - 1 to y) to this[x - 1][y])
+        if (x + 1 in indices) adjacent.add((x + 1 to y) to this[x + 1][y])
+        if (y - 1 in this[0].indices) adjacent.add((x to y - 1) to this[x][y - 1])
+        if (y + 1 in this[0].indices) adjacent.add((x to y + 1) to this[x][y + 1])
         return adjacent.toList()
     }
 }
