aedbca3b77fe187d9b76077f262926c290df2ce1
Aidan Dagnall <aidandagnall@gmail.com>
2021-12-16 12:49:30 +0000

    :art: Clean day 16 and improve testing

--- 2021/src/main/kotlin/days/Day16.kt
+++ 2021/src/main/kotlin/days/Day16.kt
@@ -1,41 +1,32 @@
 package days
 
-import days.Day16.Packet.Companion.packets
-
 class Day16 : Day(16) {
 
-    class Packet(var input: MutableList<Char>) {
-        companion object {
-            @JvmStatic val packets = mutableListOf<Packet>()
-        }
-        val version = input.take(3).joinToString("").toInt(2)
-        private val type = input.drop(3).take(3).joinToString("").toInt(2)
+    class Packet(input: MutableList<Char>) {
+        constructor(str : String) : this(convertInput(str))
+        private val version : Int = input.takeRemove(3).joinToString("").toInt(2)
+        private val type = input.takeRemove(3).joinToString("").toInt(2)
 
         private val payload : Long = if (type == 4) {
-            val inputs: List<List<Char>> = input.drop(6).chunked(5)
-            val values: MutableList<List<Char>> = inputs.takeWhile { it[0] == '1' }.toMutableList()
-            values += inputs.first { it[0] == '0' }
-            input = input.drop(6).drop(5 * values.size).toMutableList()
-            values.joinToString("") { it.drop(1).joinToString("") }.toLong(2)
-        } else { 0 }
+            input.takeRemove((input.chunked(5).indexOfFirst { it[0] == '0' } + 1) * 5)
+                .chunked(5).joinToString("") { it.drop(1).joinToString("") }.toLong(2)
+        } else {
+            0
+        }
 
         private val subPackets = mutableListOf<Packet>().apply {
             if (type == 4) return@apply
-            if (input.drop(6)[0] == '0') {
-                val length = input.drop(7).take(15).joinToString("").toInt(2)
-                val shortInput = input.drop(22).take(length).toMutableList()
-                input = input.drop(22 + length).toMutableList()
-                add(Packet(shortInput))
-                while (last().input.isNotEmpty()) add(Packet(last().input))
+            if ('0' in input.takeRemove(1)) {
+                val length = input.takeRemove(15).joinToString("").toInt(2)
+                val originalLength = input.size
+                while (input.size > originalLength - length) add(Packet(input))
             } else {
-                val num = input.drop(7).take(11).joinToString("").toInt(2)
-                input = input.drop(18).toMutableList()
-                add(Packet(input))
-                (1 until num).forEach { _ -> add(Packet(last().input)) }
-                input = last().input
+                val num = input.takeRemove(11).joinToString("").toInt(2)
+                (0 until num).forEach { _ -> add(Packet(input)) }
             }
         }
 
+        val sumVersion : Int = version + subPackets.sumOf { it.sumVersion }
         var result : Long = when (type) {
             0 -> subPackets.sumOf { it.result }
             1 -> subPackets.fold(1) { acc, next -> acc * next.result }
@@ -47,34 +38,26 @@ class Day16 : Day(16) {
             7 -> subPackets.run { if (get(0).result == get(1).result) 1L else 0L }
             else -> { println("-- I FOUND AN ERROR --"); 99999999L }
         }
+
         private fun <T> MutableList<T>.takeRemove(n: Int) : List<T> {
             val removed : List<T> = this.take(n)
-            this.removeAll(this.take(n))
+            (0 until n).forEach { _ -> this.removeFirst() }
             return removed
         }
-        private fun <T> MutableList<T>.dropRemove(n: Int) : List<T> {
-            this.removeAll(this.take(n))
-            return this.toList()
-        }
-        init { packets.add(this) }
     }
+    override fun part1() = Packet(inputList.first()).sumVersion
 
-    val input: MutableList<Char> = inputList.first()
-        .flatMap {
-            Integer.parseInt(it.toString(), 16).toString(2).padStart(4, '0')
-                .split("")
-                .filter { it != "" }
-                .map { it[0] }
-        }.toMutableList()
+    override fun part2() = Packet(inputList.first()).result
 
-    init {
-        Packet(input)
-        while(packets.last().input.all { it != '0' } && packets.last().input.isNotEmpty()) {
-            Packet(packets.last().input)
+    companion object {
+        @JvmStatic
+        fun convertInput(str: String): MutableList<Char> {
+            return str.flatMap { hexDigit ->
+                Integer.parseInt(hexDigit.toString(), 16)
+                    .toString(2)
+                    .padStart(4, '0')
+                    .toList()
+            }.toMutableList()
         }
     }
-
-    override fun part1() = packets.sumOf { it.version }
-
-    override fun part2() = packets.last().result
 }
--- 2021/src/test/kotlin/days/Day16Test.kt
+++ 2021/src/test/kotlin/days/Day16Test.kt
@@ -8,12 +8,22 @@ class Day16Test {
 
     @Test
     fun testPartOne() {
-        assertEquals(31, day.part1())
+        assertEquals(16, Day16.Packet("8A004A801A8002F478").sumVersion)
+        assertEquals(12, Day16.Packet("620080001611562C8802118E34").sumVersion)
+        assertEquals(23, Day16.Packet("C0015000016115A2E0802F182340").sumVersion)
+        assertEquals(31, Day16.Packet("A0016C880162017C3686B18A3D4780").sumVersion)
     }
 
 
     @Test
     fun testPartTwo() {
-        assertEquals(0L, day.part2())
+        assertEquals(3, Day16.Packet("C200B40A82").result)
+        assertEquals(54, Day16.Packet("04005AC33890").result)
+        assertEquals(7, Day16.Packet("880086C3E88112").result)
+        assertEquals(9, Day16.Packet("CE00C43D881120").result)
+        assertEquals(1, Day16.Packet("D8005AC2A8F0").result)
+        assertEquals(0, Day16.Packet("F600BC2D8F").result)
+        assertEquals(0, Day16.Packet("9C005AC2F8F0").result)
+        assertEquals(1, Day16.Packet("9C0141080250320F1802104A08").result)
     }
 }
