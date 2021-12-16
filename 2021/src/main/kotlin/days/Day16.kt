0f3867f6a67ba385dcff1ee57cb308c166bb063a
Aidan Dagnall <aidandagnall@gmail.com>
2021-12-16 12:59:33 +0000

    :art: Clean day 16

--- 2021/src/main/kotlin/days/Day16.kt
+++ 2021/src/main/kotlin/days/Day16.kt
@@ -2,17 +2,21 @@ package days
 
 class Day16 : Day(16) {
 
+    override fun part1() = Packet(inputList.first()).sumVersion
+
+    override fun part2() = Packet(inputList.first()).result
+
     class Packet(input: MutableList<Char>) {
-        constructor(str : String) : this(convertInput(str))
-        private val version : Int = input.takeRemove(3).joinToString("").toInt(2)
+        constructor(str: String) : this(convertInput(str))
+
+        private val version: Int = input.takeRemove(3).joinToString("").toInt(2)
         private val type = input.takeRemove(3).joinToString("").toInt(2)
 
-        private val payload : Long = if (type == 4) {
+        private val payload: Long = if (type == 4) getPayload(input) else 0
+
+        private fun getPayload(input: MutableList<Char>) =
             input.takeRemove((input.chunked(5).indexOfFirst { it[0] == '0' } + 1) * 5)
                 .chunked(5).joinToString("") { it.drop(1).joinToString("") }.toLong(2)
-        } else {
-            0
-        }
 
         private val subPackets = mutableListOf<Packet>().apply {
             if (type == 4) return@apply
@@ -26,8 +30,8 @@ class Day16 : Day(16) {
             }
         }
 
-        val sumVersion : Int = version + subPackets.sumOf { it.sumVersion }
-        var result : Long = when (type) {
+        val sumVersion: Int = version + subPackets.sumOf { it.sumVersion }
+        var result: Long = when (type) {
             0 -> subPackets.sumOf { it.result }
             1 -> subPackets.fold(1) { acc, next -> acc * next.result }
             2 -> subPackets.minOf { it.result }
@@ -36,28 +40,27 @@ class Day16 : Day(16) {
             5 -> subPackets.run { if (get(0).result > get(1).result) 1L else 0L }
             6 -> subPackets.run { if (get(0).result < get(1).result) 1L else 0L }
             7 -> subPackets.run { if (get(0).result == get(1).result) 1L else 0L }
-            else -> { println("-- I FOUND AN ERROR --"); 99999999L }
+            else -> {
+                println("-- I FOUND AN ERROR --"); 99999999L
+            }
         }
 
-        private fun <T> MutableList<T>.takeRemove(n: Int) : List<T> {
-            val removed : List<T> = this.take(n)
+        private fun <T> MutableList<T>.takeRemove(n: Int): List<T> {
+            val removed: List<T> = this.take(n)
             (0 until n).forEach { _ -> this.removeFirst() }
             return removed
         }
-    }
-    override fun part1() = Packet(inputList.first()).sumVersion
 
-    override fun part2() = Packet(inputList.first()).result
-
-    companion object {
-        @JvmStatic
-        fun convertInput(str: String): MutableList<Char> {
-            return str.flatMap { hexDigit ->
-                Integer.parseInt(hexDigit.toString(), 16)
-                    .toString(2)
-                    .padStart(4, '0')
-                    .toList()
-            }.toMutableList()
+        companion object {
+            @JvmStatic
+            fun convertInput(str: String): MutableList<Char> {
+                return str.flatMap { hexDigit ->
+                    Integer.parseInt(hexDigit.toString(), 16)
+                        .toString(2)
+                        .padStart(4, '0')
+                        .toList()
+                }.toMutableList()
+            }
         }
     }
 }
