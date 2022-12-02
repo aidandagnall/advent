6b07de1743316aca5c0b7d267227bcbd88c2f64b
Aidan Dagnall <aidandagnall@gmail.com>
2022-12-02 06:29:47 +0000

    :art: Improve and Clean up day 2

--- 2022/src/main/kotlin/days/Day02.kt
+++ 2022/src/main/kotlin/days/Day02.kt
@@ -2,48 +2,26 @@ package days
 
 class Day02 : Day(2) {
 
+    // Rock     -> 0,  Loss -> 0
+    // Paper    -> 1,  Draw -> 1
+    // Scissors -> 2,  Win  -> 2
     private val input = inputList
         .map { it.split(" ") }
-        .map { (a, b) -> HandShape((a.first() - 'A')) to b.first() - 'X' }
-
-    private val resultScore = mapOf (-1 to 0, 0 to 3, 1 to 6)
+        .map { (a, b) -> a.first() - 'A' to b.first() - 'X' }
 
     override fun part1() : Any {
-        return input.sumOf { (a, b) ->
-            HandShape(b).let {
-                it.score + resultScore[it.compareTo(a)]!!
-            }
-        }
+        return input.sumOf { (a, b) -> b + 1 + 3 * compareHands(b, a) }
     }
 
     override fun part2() : Any {
-        return input.sumOf { (a, b) -> resultScore[b - 1]!! + a.getResult(b).score }
+        return input.sumOf { (a, b) -> 3 * b + getResult(a, b) + 1}
     }
 
-    data class HandShape(val shape: Int): Comparable<HandShape> {
-        val score = shape + 1
-        private val char = when (shape) {
-            0 -> 'R'
-            1 -> 'P'
-            2 -> 'S'
-            else -> 'E'
+    private fun compareHands(a: Int, b: Int): Int = (4 + a - b) % 3
+    private fun getResult(shape: Int, result: Int): Int = when (result) {
+            0 -> (3 + shape - 1) % 3
+            1 -> (shape)
+            2 -> (shape + 1) % 3
+            else -> 0
         }
-
-        override fun compareTo(other: HandShape): Int {
-            return when (this.char to other.char) {
-                'R' to 'S', 'S' to 'P', 'P' to 'R' -> 1
-                'R' to 'P', 'P' to 'S', 'S' to 'R' -> -1
-                else -> 0
-            }
-        }
-
-        fun getResult(result: Int): HandShape {
-            return HandShape(when (result) {
-                0 -> (3 + shape - 1) % 3
-                1 -> (shape)
-                2 -> (shape + 1) % 3
-                else -> 0
-            })
-        }
-    }
 }
