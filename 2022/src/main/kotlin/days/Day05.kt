508299ec46f9b18e82b9a1f42126b1505214f428
Aidan Dagnall <aidandagnall@gmail.com>
2022-12-05 13:33:03 +0000

    🎨 Improve day 5

--- 2022/src/main/kotlin/days/Day05.kt
+++ 2022/src/main/kotlin/days/Day05.kt
@@ -3,33 +3,25 @@ package days
 class Day05 : Day(5) {
 
     private val instructionRegex = Regex("""move (\d+) from (\d+) to (\d+)""")
+    private val initialStack = inputList.take(inputList.indexOf("")).let { input ->
+        val state = input.dropLast(1)
+        val numStacks = input.last()
+            .split(" ")
+            .count { it != " " && it.isNotEmpty() }
 
-    private val initialStack = inputList.indexOfFirst{ it == "" }.let {
-        val initial = inputList.take(it - 1)
-        val numStacks = inputList[it - 1].takeLast(3).trim().toInt()
-
-        val stacks = List(numStacks) { mutableListOf<Char>()}
-
-        initial.forEach {
-            (0 until numStacks * 4 step 4).mapIndexed inner@{ stackIndex, charIndex ->
-                if (charIndex + 3 > it.length) return@forEach
-                val str = it.substring(charIndex, charIndex + 3).trim()
-                if (str.isEmpty()) return@inner
-                stacks[stackIndex].add(str.drop(1).first())
-            }
+        List(numStacks) { stack ->
+            state.map { it.getOrElse(stack * 4 + 1) { ' ' } }.filter {it != ' '}
         }
-
-        stacks.map { stack -> stack.toList() }
     }
 
-    private val instructions = inputList.drop(inputList.indexOf("") + 1)
-        .map {
+    private val instructions = inputList.drop(inputList.indexOf("") + 1).map {
             instructionRegex.find(it)!!.let { matches ->
-                matches.groupValues.let {(str, a, b, c) ->
+                matches.groupValues.let {(_, a, b, c) ->
                     Triple(a.toInt(), b.toInt(), c.toInt())
                 }
             }
         }
+
     override fun part1() : Any {
         return instructions.fold(initialStack.toMutableList()) { acc, (num, src , dest) ->
             acc.apply {
