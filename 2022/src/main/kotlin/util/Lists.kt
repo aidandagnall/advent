d15ee17d6b25efd032f324dc0634e6e0f4853ca6
Aidan Dagnall <aidandagnall@gmail.com>
2022-12-07 06:30:24 +0000

    ✨ Add list helper

--- 2022/src/main/kotlin/util/Lists.kt
+++ 2022/src/main/kotlin/util/Lists.kt
@@ -1,4 +1,10 @@
 package util
 
-class Lists {
+inline fun <reified T> transpose(xs: List<List<T>>): List<List<T>> {
+    return List(xs[0].size) { j ->
+        List(xs.size) { i ->
+            xs[i][j]
+        }
+    }
 }
+
