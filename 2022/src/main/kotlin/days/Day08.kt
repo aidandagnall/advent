c414c8272e158bef9f5bfab7f00be66c4cecd8b1
Aidan Dagnall <aidandagnall@gmail.com>
2022-12-08 17:37:47 +0000

    :art: Improve readability of Day 8 pt 2

--- 2022/src/main/kotlin/days/Day08.kt
+++ 2022/src/main/kotlin/days/Day08.kt
@@ -6,9 +6,9 @@ class Day08 : Day(8) {
             (trees[0].indices).map { y ->
                 trees[x][y] to listOf(                         // height to trees in each direction (nearest first)
                     trees[x].take(y).reversed(),               // trees to the left
-                    trees[x].drop(y + 1),                   // trees to the right
+                    trees[x].drop(y + 1),                      // trees to the right
                     trees.map { it[y] }.take(x).reversed(),    // trees above
-                    trees.map { it[y] }.drop(x + 1),        // trees below
+                    trees.map { it[y] }.drop(x + 1),           // trees below
                 )
             }
         }.flatten()
@@ -23,7 +23,7 @@ class Day08 : Day(8) {
     override fun part2() : Any {
         return input.maxOf { (height, directions) ->
             directions.map { d ->
-                d.takeWhile { it < height }.count() + if (d.any { it >= height}) 1 else 0
+                d.indexOfFirst { it >= height }.let { if (it == -1) d.size else it + 1 }
             }.fold(1) { acc, i -> acc * i}
         }
     }
