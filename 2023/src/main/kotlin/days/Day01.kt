8b34e27ee7d946ce975f15c1044c08a662fd3668
Aidan Dagnall <aidan.dagnall@transferwise.com>
2023-12-01 05:25:12 +0000

    :art: Formatting Day 1. Maybe I'll just leave it like this...

--- 2023/src/main/kotlin/days/Day01.kt
+++ 2023/src/main/kotlin/days/Day01.kt
@@ -1,29 +1,25 @@
 package days
 
 class Day01 : Day(1) {
-    override fun part1() : Any {
-        return inputList.sumOf { line ->
-            line.filter { it.isDigit() }.let {
-                (it.first().code - 48) * 10 + it.last().code - 48
-            }
+    override fun part1() : Any = inputList.sumOf { line ->
+        line.filter { it.isDigit() }.let {
+            (it.first().digitToInt()) * 10 + it.last().digitToInt()
         }
     }
 
     // this is without a doubt some of the worst code I have ever written
-    override fun part2() : Any {
-        return inputList.sumOf { line ->
-            line.replace("one", "one1one")
-                .replace("two", "two2two")
-                .replace("three", "three3three")
-                .replace("four", "four4four")
-                .replace("five", "five5five")
-                .replace("six", "six6six")
-                .replace("seven", "seven7seven")
-                .replace("eight", "eight8eight")
-                .replace("nine", "nine9nine")
-                .filter { it.isDigit() }.let {
-                (it.first().code - 48) * 10 + it.last().code - 48
-            }
+    override fun part2() : Any =inputList.sumOf { line ->
+        line.replace("one", "one1one")
+            .replace("two", "two2two")
+            .replace("three", "three3three")
+            .replace("four", "four4four")
+            .replace("five", "five5five")
+            .replace("six", "six6six")
+            .replace("seven", "seven7seven")
+            .replace("eight", "eight8eight")
+            .replace("nine", "nine9nine")
+            .filter { it.isDigit() }.let {
+            it.first().digitToInt() * 10 + it.last().digitToInt()
         }
     }
 }
