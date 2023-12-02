8eddf17f965473abaecd17cd937d1e79428cc50a
Aidan Dagnall <aidandagnall@gmail.com>
2023-12-02 22:19:14 +0000

    :art: Clean up regex

--- 2023/src/main/kotlin/days/Day02.kt
+++ 2023/src/main/kotlin/days/Day02.kt
@@ -3,11 +3,13 @@ package days
 class Day02 : Day(2) {
     data class Game(val number: Int, val red: Int, val green: Int, val blue: Int)
     private val requirements = inputList.map { line -> Game(
-        """Game (\d+):""".toRegex().find(line)!!.destructured.component1().toInt(),
-        """(\d+) red""".toRegex().findAll(line).maxOf { it.destructured.component1().toInt() },
-        """(\d+) green""".toRegex().findAll(line).maxOf { it.destructured.component1().toInt() },
-        """(\d+) blue""".toRegex().findAll(line).maxOf { it.destructured.component1().toInt() },
+        "Game (\\d+):".toRegex().find(line)!!.groupValues[1].toInt(),
+        "(\\d+) red".toRegex().findAll(line).maxOf { it.groupValues[1].toInt() },
+        "(\\d+) green".toRegex().findAll(line).maxOf { it.groupValues[1].toInt() },
+        "(\\d+) blue".toRegex().findAll(line).maxOf { it.groupValues[1].toInt() },
     ) }
-    override fun part1() : Any = requirements.filter { it.red <= 12 && it.green <= 13 && it.blue <= 14 }.sumOf { it.number }
+    override fun part1() : Any = requirements.filter {
+        it.red <= 12 && it.green <= 13 && it.blue <= 14
+    }.sumOf { it.number }
     override fun part2() : Any = requirements.sumOf { it.red * it.green * it.blue }
 }
