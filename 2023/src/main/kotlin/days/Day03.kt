ef07df3a971ebdb210d0751a6a67aafe14d7d343
Aidan Dagnall <aidandagnall@gmail.com>
2023-12-03 06:25:33 +0000

    :art: Add regex to parse and cleanup

--- 2023/src/main/kotlin/days/Day03.kt
+++ 2023/src/main/kotlin/days/Day03.kt
@@ -1,71 +1,37 @@
 package days
 
-import util.plus
-
 class Day03 : Day(3) {
 
-    val symbols = inputList.mapIndexed { y, it ->
-        it.mapIndexed { x, c ->
-            if (!c.isDigit() && c != '.')
-                (x to y) to c
+    private val symbols = inputList.mapIndexed { y, it ->
+        it.mapIndexedNotNull { x, c ->
+            if (!c.isDigit() && c != '.') (x to y) to c
             else null
-        }.filterNotNull()
+        }
     }.flatten().toMap()
 
-    val numbers = inputList.mapIndexed { y, it ->
-        val n = mutableListOf<Pair<List<Pair<Int,Int>>,Int>>()
-        var bufferN = 0
-        var bufferCoords = mutableListOf<Pair<Int,Int>>()
-
-        it.forEachIndexed { x, c ->
-            if (c.isDigit()) {
-                bufferN *= 10
-                bufferN += c.digitToInt()
-                bufferCoords.add(x to y)
-            }
-            else {
-                if (bufferN != 0) {
-                    n += bufferCoords to bufferN
-                    bufferN = 0
-                    bufferCoords = mutableListOf()
-                }
-            }
+    private val numbers = inputList.flatMapIndexed { y, line ->
+        "(\\d+)".toRegex().findAll(line).map { match ->
+            match.range.map { it to y } to match.value.toInt()
         }
-
-        if (bufferN != 0) {
-            n += bufferCoords to bufferN
-            bufferN = 0
-            bufferCoords = mutableListOf()
-        }
-
-        n.toList()
-    }.flatten()
-
-    override fun part1() : Any {
-        return numbers
-            .filter { (coords, _) ->
-                coords.any {
-                    (-1..1).any {x ->
-                        (-1..1).any { y ->
-                            it.first + x to it.second + y in symbols.keys
-                        }
-                    }
-                }
-            }.sumOf { it.second }
-
     }
 
-    override fun part2() : Any {
-        return symbols.filter { (_, v) -> v == '*' }
+    override fun part1() : Any =
+        numbers
+            .filter { (coords, _) -> coords.any { n -> n.neighbours().any { it in symbols } } }
+            .sumOf { it.second }
+
+
+    override fun part2() : Any =
+        symbols.filter { (_, v) -> v == '*' }
             .map { (k, _) ->
-                numbers.filter {
-                    (-1..1).any {x ->
-                        (-1..1).any { y ->
-                            k + (x to y) in it.first
-                        }
-                    }
-                }
+                numbers.filter { (coords, _) -> k.neighbours().any { it in coords} }
             }.filter { it.count() == 2 }
             .sumOf { (a, b) -> a.second * b.second }
-    }
+
+    private fun Pair<Int,Int>.neighbours(): List<Pair<Int,Int>> =
+        (-1..1).flatMap { x ->
+            (-1..1).map { y ->
+                this.first + x to this.second + y
+            }
+        }
 }
