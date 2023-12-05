898338d56aca852feb85a7092970a4c3a9ebf434
Aidan Dagnall <aidandagnall@gmail.com>
2023-12-05 09:00:15 +0000

    :art: Clean Day 4

--- 2023/src/main/kotlin/days/Day04.kt
+++ 2023/src/main/kotlin/days/Day04.kt
@@ -4,23 +4,21 @@ import kotlin.math.pow
 
 class Day04 : Day(4) {
     private val games = inputList.filter { it != "" }.associate {
-        it.split(":", "|").let { (number, winners, played) ->
-            number.removePrefix("Card").trim().toInt() to (
-                winners.split(" ").filter { it != "" }.map { it.toInt() } to
-                played.split(" ").filter { it != "" }.map { it.toInt() }
-            )
+        it.split(":", "|").let { (number, winnersRaw, yoursRaw) ->
+            val winners = winnersRaw.split(" ").filter { it != "" }.map { it.toInt() }
+            val yours = yoursRaw.split(" ").filter { it != "" }.map { it.toInt() }
+
+            number.removePrefix("Card").trim().toInt() to winners.count { it in yours }
         }
     }
 
-    override fun part1() : Any = games.values.sumOf { (winners, played) ->
-            winners.count { it in played }.let {
-                if (it == 0) 0
-                else 2.0.pow(it - 1).toInt()
-            }
+    override fun part1() : Any = games.values.sumOf {
+            if (it == 0) 0
+            else 2.0.pow(it - 1).toInt()
         }
 
-    override fun part2() : Any = games.toList().fold(mapOf<Int,Int>()) { acc, (number, game) ->
-            val changes: List<Pair<Int,Int>> = (1..game.first.count { it in game.second}).map { count ->
+    override fun part2() : Any = games.toList().fold(mapOf<Int,Int>()) { acc, (number, matches) ->
+            val changes: List<Pair<Int,Int>> = (1..matches).map { count ->
                 number + count to (acc.getOrDefault(number + count, 0) + acc.getOrDefault(number, 0) + 1)
             }
             acc.plus(number to acc.getOrDefault(number, 0) + 1).plus(changes)
