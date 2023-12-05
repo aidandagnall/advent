619b16426eca81880528bd0f83fdd0b2fc342204
Aidan Dagnall <aidandagnall@gmail.com>
2023-12-05 17:42:24 +0000

    :thinking: There's something clever I can do here...
    I just don't know what

--- 2023/src/main/kotlin/days/Day05.kt
+++ 2023/src/main/kotlin/days/Day05.kt
@@ -3,80 +3,65 @@ package days
 import kotlin.math.min
 
 class Day05 : Day(5) {
+    data class Mapper(val dest: Long, val src: Long, val size: Long) {
+        val srcRange: LongRange
+            get() = src..<src + size
 
-    val seeds = inputList.first().removePrefix("seeds: ").split(" ").map { it.toLong() }
-    val maps = inputGroupedList.drop(1).map {
-        it.drop(1).map {
+        val destRange: LongRange
+            get() = dest ..< dest + size
+
+        fun map(input: Long): Long = if (input in srcRange) input - src + dest else input
+
+        fun subRanges(start: Long, end: Long): List<LongRange> {
+            if (start > src + size || end < src) return emptyList()
+
+            if (start < src && src + size < end) return listOf(start..< src, destRange, src + size ..< end)
+
+            else return emptyList()
+//            if ()
+        }
+    }
+
+    private val seeds = inputList.first().removePrefix("seeds: ").split(" ").map { it.toLong() }
+    private val maps = inputGroupedList.drop(1).map { group ->
+        group.drop(1).map {
             it.split(" ").let { (dest, src, len) ->
-                Triple(dest.toLong(), src.toLong(), len.toLong())
+                Mapper(dest.toLong(), src.toLong(), len.toLong())
             }
         }
     }
+
+    // 388071289
     override fun part1() : Any {
-        val seeds = inputList.first().removePrefix("seeds: ").split(" ").map { it.toLong() }
         return seeds.minOf { num ->
             maps.fold(num) { acc, map ->
-                map.firstOrNull { (dest, src, len) ->
+                map.firstOrNull { (_, src, len) ->
                     acc in (src..<src + len)
-                }?.let { (dest, src, len) ->
+                }?.let { (dest, src, _) ->
                     acc + (dest - src)
                 } ?: acc
             }
         }
     }
 
-    fun createMappingRanges(seed: Long, range: Long, mappers: List<Triple<Long,Long,Long>>): List<Pair<Long,Long>> {
-        val out = mutableListOf<Pair<Long,Long>>()
-
-        var currentSeed = seed
-
-
-        if (seed + range < mappers.minOf { it.second }) {
-            return listOf(seed to range)
-        }
-
-        val maps = mappers
-            .filter { (_, src, len) -> currentSeed <= src + len}
-            .sortedBy { it.second }.toMutableList()
-
-
-        var currentEnd = mappers.first().second
-
-        println(" curr-> $currentSeed")
-        println(" end-> $currentEnd")
-        println(maps)
-
-        while (currentEnd < seed + range) {
-
-            if (currentSeed in maps.first().let { (_, s, r) -> s..s + r }) {
-                out.add(maps.first().first to currentEnd - currentSeed)
-                currentSeed = maps.first().second + maps.first().third
-                maps.removeFirst()
-            } else {
-                out.add(currentSeed to currentEnd - currentSeed)
-                currentSeed = currentEnd
-            }
-            currentEnd = min(maps.first().second, seed + range)
-        }
-
-        println(" out -> $out")
-        return out
-    }
-
+    // 84206669
     override fun part2() : Any {
-        val seedRanges = inputList.first().removePrefix("seeds: ").split(" ").chunked(2).map { (a, b) -> a.toLong() to b.toLong() }
+        return 0
+        val maps = maps.map { it.map { (dest, src, len) -> dest..< dest + len to src} }
 
-        val ranges = seedRanges.map { (a,b) -> a..< a + b }
+        val seedRanges = inputList.first().removePrefix("seeds: ").split(" ")
+            .chunked(2)
+            .map { (a, b) -> a.toLong() ..< a.toLong() + b.toLong() }
 
-        return (0..maps.last().maxOf { it.first }).first { num ->
-            maps.fold(num) { acc, map ->
-                map.firstOrNull { (dest, src, len) ->
-                    acc in (dest..<dest + len)
-                }?.let { (dest, src, len) ->
-                    acc - dest  + src
+        return (0..maps.last().maxOf { it.first.last }).first { num ->
+            maps.reversed().fold(num) { acc, map ->
+                map.firstOrNull { (range, src) ->
+                    acc in range
+                }?.let { (dest, src) ->
+                    acc - dest.first + src
                 } ?: acc
             }.let { seed ->
-              ranges.any { seed in it }
+                seedRanges.any { seed in it }
             }
         }
     }
