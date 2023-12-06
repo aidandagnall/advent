c9270c5de780486db9139df989cf1de2e9902729
Aidan Dagnall <aidan.dagnall@transferwise.com>
2023-12-06 09:44:53 +0000

    :art: Clean Days 5 & 6

--- 2023/src/main/kotlin/days/Day05.kt
+++ 2023/src/main/kotlin/days/Day05.kt
@@ -2,7 +2,7 @@ package days
 
 class Day05 : Day(5) {
     data class Layer(val mappers: List<Mapper>) {
-        fun map(input: Long): Long = mappers.find { input in it }?.map(input) ?: input
+        fun map(input: Long): Long = mappers.find { input in it.srcRange }?.map(input) ?: input
         fun subRanges(start: Long, size: Long): List<Pair<Long, Long>> {
             val breakpoints = mappers.flatMap { listOf(it.srcRange.first, it.srcRange.last) } + listOf(start, start + size)
 
@@ -10,19 +10,16 @@ class Day05 : Day(5) {
                 .dropWhile { it < start }.takeWhile { it <= start + size}
                 .windowed(2)
                 .map { (first, last) ->
-                    mappers.find { first in it }?.let { it.map(first) to (last - first) } ?: (first to (last - first))
+                    mappers.find { first in it.srcRange }?.let { it.map(first) to (last - first) } ?: (first to (last - first))
                 }.toList()
         }
     }
     data class Mapper(val dest: Long, val src: Long, val size: Long) {
         val srcRange: LongRange = src ..< src + size
         fun map(input: Long): Long = input - src + dest
-        operator fun contains(other: Long): Boolean = other in srcRange
     }
 
     private val seeds = inputList.first().removePrefix("seeds: ").split(" ").map { it.toLong() }
-    private val seedRanges = inputList.first().removePrefix("seeds: ").split(" ").chunked(2)
-        .map { (a, b) -> a.toLong()to b.toLong() }
     private val layers = inputGroupedList.drop(1).map { group ->
         Layer(
             group.drop(1).map {
@@ -39,8 +36,8 @@ class Day05 : Day(5) {
             }
         }
 
-    override fun part2() : Any = seedRanges.minOf {
-            layers.fold(listOf(it)) { acc, layer ->
+    override fun part2() : Any = seeds.chunked(2).minOf { (seedStart, seedSize) ->
+            layers.fold(listOf(seedStart to seedSize)) { acc, layer ->
                 acc.flatMap { (start, size) ->
                     layer.subRanges(start, size)
                 }
--- 2023/src/main/kotlin/days/Day06.kt
+++ 2023/src/main/kotlin/days/Day06.kt
@@ -2,21 +2,18 @@ package days
 
 class Day06 : Day(6) {
 
-    private val times = "(\\d+)".toRegex().findAll(inputList.first()).map { it.value.toInt() }
-    private val dists = "(\\d+)".toRegex().findAll(inputList.last()).map { it.value.toInt() }
-    private val races = times.zip(dists).toList()
+    private val races = inputList.map { line ->
+        "(\\d+)".toRegex().findAll(line).map { it.value.toLong() }
+    }.let { (times, dists) -> times.zip(dists) }.toList()
 
-    override fun part1() : Any = races.map {  (time, dist) ->
-            (0..time).count { speed ->
-                (time - speed) * speed > dist
-            }
-        }.fold(1) { acc, it -> acc * it}
+    override fun part1() : Any = races.map {  (time, dist) -> wins(time, dist) }
+        .reduce { acc, it -> acc * it}
 
-    override fun part2() : Any {
-        val sumTime = inputList.first().removePrefix("Time:").replace(" ", "").toLong()
-        val sumDist = inputList.last().removePrefix("Distance:").replace(" ", "").toLong()
-        return (0..sumTime).count { speed ->
-            (sumTime - speed) * speed > sumDist
+    override fun part2() : Any = races.unzip().let { (times, dists) ->
+            times.joinToString("").toLong() to dists.joinToString("").toLong()
+        }.let { (time, dist) -> wins(time, dist) }
+
+    private fun wins(time: Long, dist: Long): Int = (0..time).count { speed ->
+            (time - speed) * speed > dist
         }
-    }
 }
