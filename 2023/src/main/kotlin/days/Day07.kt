0187f5a2eb1adc23ca0f14417e97e1c53aa64df9
Aidan Dagnall <aidandagnall@gmail.com>
2023-12-07 22:07:17 +0000

    :art: Clean Day 7

--- 2023/src/main/kotlin/days/Day07.kt
+++ 2023/src/main/kotlin/days/Day07.kt
@@ -1,100 +1,44 @@
 package days
 
+import util.replace
+
 class Day07 : Day(7) {
     data class Hand(val cards: List<Char>, val bid: Int): Comparable<Hand> {
-        override fun compareTo(other: Hand): Int {
-            return cards.zip(other.cards).map { (a, b) ->
-                cardValues[a]!!.compareTo(cardValues[b]!!)
-            }.first { it != 0 }
-        }
-        companion object {
-            val cardValues = mapOf(
-                'A' to 14,
-                'K' to 13,
-                'Q' to 12,
-                'T' to 10,
-                '9' to 9,
-                '8' to 8,
-                '7' to 7,
-                '6' to 6,
-                '5' to 5,
-                '4' to 4,
-                '3' to 3,
-                '2' to 2,
-                'J' to 1,
-            )
-        }
+        private val handValue = handType() + cards.map {
+            when(it) {
+                'A' -> 'E'
+                'K' -> 'D'
+                'Q' -> 'C'
+                'J' -> 'B'
+                'T' -> 'A'
+                '*' -> '1'
+                else -> it
+            }
+        }.joinToString("")
+
+        override fun compareTo(other: Hand): Int = handValue.compareTo(other.handValue)
+
+        private fun handType(): String = cards.groupingBy { it }.eachCount()
+            .map { (char, count) -> if (char == '*') 0 else count }
+            .sortedDescending()
+            .let { listOf(it.first() + cards.count { it == '*' }) + it.drop(1)  }
+            .joinToString("")
+            .take(2)
+            .padEnd(2, '0')
     }
 
-    val hands = inputList.map {
+    private val hands = inputList.map {
         it.split(" ").let { (cards, bid) -> Hand(cards.toList(), bid.toInt())}
     }
 
-    override fun part1() : Any {
-        return hands.groupBy {  hand ->
-            when {
-                hand.cards.all { it == hand.cards.first()} -> 7
-                hand.cards.groupingBy { it }.eachCount().containsValue(4) -> 6
-                hand.cards.groupingBy { it }.eachCount().let { it.containsValue(3) && it.containsValue(2) } -> 5
-                hand.cards.groupingBy { it }.eachCount().containsValue(3) -> 4
-                hand.cards.groupingBy { it }.eachCount().values.count { it == 2 } == 2 -> 3
-                hand.cards.groupingBy { it }.eachCount().containsValue(2) -> 2
-                else -> 1
-            }
-        }.toList().sortedBy { it.first }.flatMap {  (value, hands) ->
-            println("$value has ${hands.count()} -> $hands")
-            hands.sortedBy { it }
-        }.also {
-            it.forEach(::println)
-        }
-            .foldIndexed(0) { index, acc, hand ->
-                println("$hand gets ${(index + 1) * hand.bid}")
-            acc + ((index + 1) * hand.bid)
-        }
-    }
+    override fun part1() : Any = hands.sorted().betSum()
 
-    fun handValue(cards: List<Char>): Int {
-        return when {
-            cards.all { it == cards.first() } -> 7
-            cards.groupingBy { it }.eachCount().containsValue(4) -> 6
-            cards.groupingBy { it }.eachCount()
-                .let { it.containsValue(3) && it.containsValue(2) } -> 5
+    override fun part2() : Any = hands
+            .map { (cards, bid) -> Hand(cards.replace('J', '*'), bid) }
+            .sorted()
+            .betSum()
 
-            cards.groupingBy { it }.eachCount().containsValue(3) -> 4
-            cards.groupingBy { it }.eachCount().values.count { it == 2 } == 2 -> 3
-            cards.groupingBy { it }.eachCount().containsValue(2) -> 2
-            else -> 1
-        }
-    }
-
-    override fun part2() : Any {
-        return hands.groupBy {  hand ->
-            if ('J' !in hand.cards)
-                return@groupBy handValue(hand.cards)
-
-            val count = hand.cards.count { it == 'J' }
-            val dests = Hand.cardValues.keys.filter { it != 'J' }
-
-            val options = mutableListOf<List<Char>>(hand.cards)
-
-            (1..count).forEach {
-                val remaining = options
-                options.addAll(
-                    remaining.flatMap { hand ->
-                        dests.map { card -> hand.indexOf('J').let { index -> hand.take(index) + card + hand.drop(index + 1) }}
-                    }
-                )
-                println(options)
-            }
-
-            options.filter { 'J' !in it }.maxOf { handValue(it) }
-        }.toList().sortedBy { it.first }.flatMap {  (value, hands) ->
-            println("$value has ${hands.count()} -> $hands")
-            hands.sortedBy { it }
-        }
-            .foldIndexed(0) { index, acc, hand ->
-                println("$hand gets ${(index + 1) * hand.bid}")
-                acc + ((index + 1) * hand.bid)
-            }
+    private fun List<Hand>.betSum(): Int = foldIndexed(0) { i, acc, hand ->
+        acc + ((i + 1) * hand.bid)
     }
 }
