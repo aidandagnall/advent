0b2b41fb5f21f56c0069be063a37d718eb7f7899
Aidan Dagnall <aidandagnall@gmail.com>
2023-12-08 05:38:46 +0000

    :hammer: (spoilers) Add util

--- 2023/src/main/kotlin/util/Numeric.kt
+++ 2023/src/main/kotlin/util/Numeric.kt
@@ -2,7 +2,17 @@ package util
 
 fun Int.positiveMod(other: Int) = if (this % other < 0) other + (this % other) else this % other
 fun Int.lcm(b: Int): Int = this * (b / this.gcd(b))
+fun Long.lcm(b: Long): Long = this * (b / this.gcd(b))
 fun Int.gcd(b: Int): Int {
+    var (left, right) = this to b
+    while (right > 0) {
+        val temp = right
+        right = left % right
+        left = temp
+    }
+    return left
+}
+fun Long.gcd(b:Long): Long{
     var (left, right) = this to b
     while (right > 0) {
         val temp = right
