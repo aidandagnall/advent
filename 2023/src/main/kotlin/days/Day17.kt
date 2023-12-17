96aac11ce26956e20913cf37608581da235ccee7
Aidan Dagnall <aidan.dagnall@transferwise.com>
2023-12-17 21:34:01 +0000

    :art: Clean Day 17

--- 2023/src/main/kotlin/days/Day17.kt
+++ 2023/src/main/kotlin/days/Day17.kt
@@ -1,214 +1,88 @@
 package days
 
-import util.manhattan
+import util.plus
 import java.util.Comparator
 import java.util.PriorityQueue
 
-// 923 too high
-
 class Day17 : Day(17) {
+    enum class Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT;
 
-    val nodes = inputList.flatMapIndexed { y, line ->
+        fun opposites(): List<Direction> = when(this) {
+            UP, DOWN -> listOf(LEFT, RIGHT)
+            LEFT, RIGHT -> listOf(UP, DOWN)
+        }
+
+        fun toVector(): Pair<Int,Int> = when(this) {
+            UP -> 0 to -1
+            DOWN -> 0 to 1
+            LEFT -> -1 to 0
+            RIGHT -> 1 to 0
+        }
+    }
+    data class State(val coord: Pair<Int,Int>, val dir: Direction, val dirCount: Int) {
+        fun forward(): List<State> = listOf(copy(coord = coord + dir.toVector(), dirCount = dirCount + 1))
+
+        fun turns(): List<State> = dir.opposites().map {
+            State(coord + it.toVector(), it, 1)
+        }
+    }
+
+    private val nodes = inputList.flatMapIndexed { y, line ->
         line.mapIndexed { x, c ->
             (x to y) to c.digitToInt()
         }
     }.toMap()
 
-    val start = 0 to 0
-    val end = inputList.first().length - 1 to inputList.size - 1
+    private val start = 0 to 0
+    private val end = inputList.first().length - 1 to inputList.size - 1
 
-    private fun nextCrucible(current: State, history: List<State>): List<State> {
-        return when (current.dir) {
-            'U' -> when(current.dirCount) {
-                in 0..3 -> listOf(
-                    State(current.x, current.y - 1, current.dir, current.dirCount + 1)
-                )
-                in 4..9 -> listOf(
-                    State(current.x, current.y - 1, current.dir, current.dirCount + 1),
-                    State(current.x + 1, current.y, 'R', 1),
-                    State(current.x - 1, current.y, 'L', 1)
-                )
-                else -> listOf(
-                    State(current.x + 1, current.y, 'R', 1),
-                    State(current.x - 1, current.y, 'L', 1)
-                )
-            }
-            'D' -> when(current.dirCount) {
-                in 0..3 -> listOf(
-                    State(current.x, current.y + 1, current.dir, current.dirCount + 1)
-                )
-                in 4..9 -> listOf(
-                    State(current.x, current.y + 1, current.dir, current.dirCount + 1),
-                    State(current.x + 1, current.y, 'R', 1),
-                    State(current.x - 1, current.y, 'L', 1)
-                )
-                else -> listOf(
-                    State(current.x + 1, current.y, 'R', 1),
-                    State(current.x - 1, current.y, 'L', 1)
-                )
-            }
-            'L' -> when(current.dirCount) {
-                in 0..3 -> listOf(
-                    State(current.x - 1, current.y, current.dir, current.dirCount + 1)
-                )
-                in 4..9 -> listOf(
-                    State(current.x - 1, current.y, current.dir, current.dirCount + 1),
-                    State(current.x, current.y + 1, 'D', 1),
-                    State(current.x, current.y - 1, 'U', 1)
-                )
-                else -> listOf(
-                    State(current.x, current.y + 1, 'D', 1),
-                    State(current.x, current.y - 1, 'U', 1)
-                )
-            }
-            else -> when(current.dirCount) {
-                in 0..3 -> listOf(
-                    State(current.x + 1, current.y, current.dir, current.dirCount + 1)
-                )
-                in 4..9 -> listOf(
-                    State(current.x + 1, current.y, current.dir, current.dirCount + 1),
-                    State(current.x, current.y + 1, 'D', 1),
-                    State(current.x, current.y - 1, 'U', 1)
-                )
-                else -> listOf(
-                    State(current.x, current.y + 1, 'D', 1),
-                    State(current.x, current.y - 1, 'U', 1)
-                )
-            }
-        }
+    private fun crucibleNeighbours(current: State): List<State> =
+        current.turns() + if (current.dirCount <= 2)  current.forward() else emptyList()
+
+    private fun ultraCrucibleNeighbours(current: State): List<State> = when(current.dirCount) {
+        in 0..3 -> current.forward()
+        in 4..9 -> current.turns() + current.forward()
+        else -> current.turns()
     }
 
-    private fun next(current: State, history: List<State>): List<State> {
-        return when (current.dir) {
-            'U' -> if (history.takeLast(3).all { it.dir == 'U' }) {
-                emptyList()
-            } else  {
-                listOf(State(current.x, current.y - 1, current.dir, current.dirCount + 1))
-            } +
-                listOf(
-                    State(current.x + 1, current.y, 'R', 0),
-                    State(current.x - 1, current.y, 'L', 0)
-                )
-            'D' -> if (history.takeLast(3).all { it.dir == 'D' }) {
-                emptyList()
-            } else  {
-                listOf(State(current.x, current.y + 1, current.dir, current.dirCount + 1))
-            } +
-                    listOf(
-                        State(current.x + 1, current.y, 'R', 0),
-                        State(current.x - 1, current.y, 'L', 0)
-                    )
-            'L' -> if (history.takeLast(3).all { it.dir == 'L' }) {
-                emptyList()
-            } else  {
-                listOf(State(current.x - 1, current.y, current.dir, current.dirCount + 1))
-            } +
-                    listOf(
-                        State(current.x, current.y + 1, 'D', 0),
-                        State(current.x, current.y- 1, 'U', 0)
-                    )
-            else -> if (history.takeLast(3).all { it.dir == 'R' }) {
-                emptyList()
-            } else  {
-                listOf(State(current.x + 1, current.y , current.dir, current.dirCount + 1))
-            } +
-                    listOf(
-                        State(current.x, current.y+ 1, 'D', 0),
-                        State(current.x, current.y - 1, 'U', 0)
-                    )
-        }
-    }
+    private fun search(start: Pair<Int,Int>, end: Pair<Int,Int>, next: (State) -> List<State>, runOff: Int = 0): Int {
+        val scores = Direction.entries.associate {
+            State(start, it, 0) to 0
+        }.toMutableMap()
 
-    private fun path(from: Map<State,State>, dest: State): List<State> {
-//        println("Getting path")
-        var current = dest
-        val total = mutableListOf(dest)
-//        println(from)
-        while(current in from) {
-            current = from[current]!!
-            total.add(0, current)
-        }
-//        println("Done getting path")
-        return total.toList()
-    }
-
-    data class State(val x: Int, val y: Int, val dir: Char, val dirCount: Int)
-    private fun astar(start: Pair<Int,Int>, end: Pair<Int,Int>): Int {
-        val cameFrom = mutableMapOf<State, State>()
-
-        val gScores = mutableMapOf(
-            State(start.first, start.second, 'U', 0) to 0,
-            State(start.first, start.second, 'D', 0) to 0,
-            State(start.first, start.second, 'L', 0) to 0,
-            State(start.first, start.second, 'R', 0) to 0,
+        val queue: PriorityQueue<State> = PriorityQueue(1000, Comparator.comparingInt{ scores[it] ?: 100000 })
+        val seen = mutableSetOf(
+            State(start, Direction.RIGHT, 0),
+            State(start, Direction.DOWN, 0)
         )
+        queue.addAll(seen)
 
-        val openSet: PriorityQueue<State> = PriorityQueue(1000, Comparator.comparingInt{ gScores[it] ?: 100000 })
-        openSet.add(State(start.first, start.second, 'R', 0))
+        while(queue.isNotEmpty()) {
+            val current = queue.poll()
 
-        val openSetSet = mutableSetOf<State>()
-        openSetSet.add(State(start.first, start.second, 'R', 0))
+            if (current.coord == end) return scores[current]!!
 
-        while(openSet.isNotEmpty()) {
-//            println(openSet)
-            val current = openSet.poll()
-            openSetSet.remove(current)
-
-            if (current.x to current.y == end) {
-                val path = path(cameFrom, current)
-                println()
-                println(path.map { it to gScores[it] })
-
-                println(gScores)
-
-                println()
-
-                println((start.second..end.second).joinToString("\n") { y ->
-                    (start.first..end.first).joinToString("") { x ->
-                        if (x to y in path.map { it.x to it.y }) path.find { it.x == x && it.y == y }!!.dir.let {
-                            when(it) {
-                                'U' -> '^'
-                                'D' -> 'v'
-                                'L' -> '<'
-                                else -> '>'
-                            }
-                        }.toString() else nodes[x to y].toString()
-                    }
-                })
-                println(nodes[end])
-                return gScores[current]!!
-            }
-
-            nextCrucible(current, path(cameFrom, current))
-                .filter { (x, y, _) -> x in 0..end.first && y in 0..end.second }
+            next(current)
+                .filter { it.coord.first in 0..end.first && it.coord.second in 0..end.second }
+                .filterNot { it.coord == end && it.dirCount < runOff }
                 .forEach {
-
-                val tent = gScores.getOrDefault(current, 100000) + nodes[it.x to it.y]!!
-                if (tent < gScores.getOrDefault(it, 100000)) {
-
-                    if (it.x to it.y == end && it.dirCount < 4) {
-                        return@forEach
-                    }
-                        cameFrom[it] = current
-                        gScores[it] = tent
-
-                        if (it !in openSetSet) {
-                            openSet.add(it)
-                            openSetSet.add(it)
+                    val tent = scores.getOrDefault(current, 100000) + nodes[it.coord]!!
+                    if (tent < scores.getOrDefault(it, 100000)) {
+                        scores[it] = tent
+                        if (it !in seen) {
+                            queue.add(it)
+                            seen.add(it)
                         }
-                }
-
+                    }
             }
-
         }
         return -1
     }
 
-    override fun part1() : Any {
-        return astar(start, end)
-    }
-
-    override fun part2() : Any {
-        return astar(start, end)
-    }
+    override fun part1() : Any = search(start, end, ::crucibleNeighbours)
+    override fun part2() : Any = search(start, end, ::ultraCrucibleNeighbours, 4)
 }
