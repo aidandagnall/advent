e8c9f285600c76349429b92020c6837b463af397
Aidan Dagnall <aidan.dagnall@transferwise.com>
2023-12-19 18:08:29 +0000

    :art: Clean Day 19

--- 2023/src/main/kotlin/days/Day19.kt
+++ 2023/src/main/kotlin/days/Day19.kt
@@ -5,7 +5,9 @@ import util.replaceAt
 class Day19 : Day(19) {
 
     sealed interface Rule
-    data class Conditional(val value: PartType, val condition: Char, val target: Int, val destination: String): Rule
+    data class Conditional(val value: PartType, val condition: Char, val target: Int, val destination: String): Rule {
+        fun check(input: Int): Boolean = if (condition == '<') input < target else input > target
+    }
     data class Base(val destination: String): Rule
 
     enum class PartType {
@@ -22,15 +24,14 @@ class Day19 : Day(19) {
         }
     }
 
-    val workflows = inputGroupedList.first().associate { line ->
+    private val workflows = inputGroupedList.first().associate { line ->
         val name = line.substringBefore('{')
         val body = line.removePrefix("$name{").removeSuffix("}")
             .split(",")
             .map {
-                if (':' !in it) {
-                    Base(it)
-                } else {
-                    """([xmas])([<>])(\d+):([a-zAR]+)""".toRegex().matchEntire(it)!!
+                when {
+                    ':' !in it -> Base(it)
+                    else -> """([xmas])([<>])(\d+):([a-zAR]+)""".toRegex().matchEntire(it)!!
                         .groupValues
                         .let {(_, a, b, c, d) ->
                             Conditional(PartType.fromString(a), b.first(), c.toInt(), d)
@@ -39,44 +40,25 @@ class Day19 : Day(19) {
             }
         name to body
     }.let {
+        // this just means we only have to handle A and R in the base case
         it + ("A" to listOf(Base("A"))) + ("R" to listOf(Base("R")))
     }
 
     private val ratings = inputGroupedList.last().map {
         """\{x=(\d+),m=(\d+),a=(\d+),s=(\d+)}""".toRegex().matchEntire(it)!!
-            .groupValues.let { (_, x, m, a, s) ->
-                listOf(
-                    x.toInt(), m.toInt(), a.toInt(), s.toInt()
-                )
-            }
+            .groupValues.drop(1).map(String::toInt)
     }
 
-    override fun part1() : Any = ratings.sumOf {
-            process(it, workflows["in"]!!)
-        }
+    override fun part1() : Any = ratings.sumOf { process(it, workflows["in"]!!) }
 
-    override fun part2() : Any = findPaths(
-            listOf(
-                (1..4000).toList(),
-                (1..4000).toList(),
-                (1..4000).toList(),
-                (1..4000).toList(),
-            ),
-            workflows["in"]!!
-        )
+    override fun part2() : Any = findPaths((1..4).map { (1..4000).toList() }, workflows["in"]!!)
 
     private tailrec fun process(values: List<Int>, rules: List<Rule>): Int {
         rules.forEach {
             when(it) {
                 is Conditional -> {
-                    val value = values[it.value.ordinal]
-                    val result = if (it.condition == '<') value < it.target else value > it.target
-                    if (result) {
-                        return when(it.destination) {
-                            "A" -> values.sum()
-                            "R" -> 0
-                            else -> process(values, workflows[it.destination]!!)
-                        }
+                    if (it.check(values[it.value.ordinal])) {
+                        return process(values, workflows[it.destination]!!)
                     }
                 }
                 is Base -> return when(it.destination) {
@@ -95,20 +77,13 @@ class Day19 : Day(19) {
             when(rule) {
                 is Conditional -> {
                     val index = rule.value.ordinal
-                    acc + if (rule.condition == '<') {
-                        findPaths(
-                            continued.replaceAt(index, continued[index].filter { it < rule.target }),
-                            workflows[rule.destination]!!
-                        ).also { continued[index].removeIf { it < rule.target } }
-                    } else {
-                        findPaths(
-                            continued.replaceAt(index, continued[index].filter { it > rule.target }),
-                            workflows[rule.destination]!!
-                        ).also { continued[index].removeIf { it > rule.target } }
-                    }
+                    acc + findPaths(
+                        continued.replaceAt(index, continued[index].filter { rule.check(it) }),
+                        workflows[rule.destination]!!
+                    ).also { continued[index].removeIf { rule.check(it) } }
                 }
                 is Base -> acc + when(rule.destination) {
-                    "A" -> continued.fold(1L) { acc, it -> acc * it.count() }
+                    "A" -> continued.fold(1L) { combinations, it -> combinations * it.count() }
                     "R" -> 0
                     else -> findPaths(continued, workflows[rule.destination]!!)
                 }
