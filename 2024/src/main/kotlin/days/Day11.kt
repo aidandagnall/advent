4eb506d765d325fc175dd03445be56da7b7759de
Aidan Dagnall <aidan.dagnall@transferwise.com>
2024-12-11 10:27:11 +0000

    :art: Clean Day 11 even more!

--- 2024/src/main/kotlin/days/Day11.kt
+++ 2024/src/main/kotlin/days/Day11.kt
@@ -10,16 +10,12 @@ class Day11 : Day(11) {
     private fun String.halve() = listOf(take(length / 2), drop(length / 2)).map { it.toLong().toString() }
 
     private fun run(times: Int) = (1..times).fold(initialRocks) { rocks, _ ->
-        buildMap(rocks.size * 2) {
-            rocks.forEach { (rock, count) ->
-                when {
-                    rock == "0" -> listOf("1")
-                    rock.length % 2 == 0 -> rock.halve()
-                    else -> listOf((rock.toLong() * 2024).toString())
-                }.forEach {
-                    put(it, this[it]?.plus(count) ?: count)
-                }
-            }
-        }
+        rocks.flatMap { (rock, count) ->
+            when {
+                rock == "0" -> listOf("1")
+                rock.length % 2 == 0 -> rock.halve()
+                else -> listOf((rock.toLong() * 2024).toString())
+            }.map { it to count }
+        }.groupBy { it.first }.mapValues { (_, v) -> v.sumOf { it.second } }
     }
 }
